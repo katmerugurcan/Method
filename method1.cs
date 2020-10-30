35eee8213e9b49fc871bfe914bf6aa0ebf6b8f12
fbcan98 <57903386+katmerugurcan@users.noreply.github.com>
2020-10-30 15:38:41 +0300

    Add files via upload

---
+++ method1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace metotornegim
+{
+        class Program
+        {
+
+        static void Main(string[] args)
+        {
+                carpim();
+
+                VizeFinal(56, 85);
+                BuyukSayi();
+            metinuzunluk();
+        }
+            public static void carpim()
+            {
+                for (int x = 1; x <= 10; x++)
+                {
+                    for (int i = 1; i <= 10; i++)
+                    {
+                        Console.Write("{0}*{1}={2}\t", i, x, (x * i));
+                    }
+                }
+            }
+            public static void metinuzunluk()
+        {
+            Console.WriteLine("Yazı Girin : ");
+            string yazi = Console.ReadLine();
+            int uzunluk = yazi.Length;
+            Console.WriteLine("Uzunlugu = " + uzunluk);
+            Console.ReadLine();
+        }
+            public static void VizeFinal(int vize, int final)
+            {
+
+                double ortalama;
+                ortalama = (vize * 40 / 100) + (final * 60 / 100);
+                Console.WriteLine("Ortalamanız: " + ortalama);
+
+            }
+
+            public static void BuyukSayi()
+            {
+                Console.WriteLine("Birinci sayiyi girin: ");
+                double i = Double.Parse(Console.ReadLine());
+
+                Console.WriteLine("ikinci sayiyi girin: ");
+                double a = Double.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Buyuk olan sayi: {(i > a ? i : a)} \n\n");
+                
+            }
+
+          
+        }
+    }
+   
